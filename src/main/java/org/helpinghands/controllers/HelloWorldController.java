15d0c2cd96cf593350591143953fe937a73882d2
Jeff <jeffnb@gmail.com>
2014-05-31 16:25:55 -0700

    Moving controller to controlers package Steve

--- src/main/java/org/helpinghands/controllers/HelloWorldController.java
+++ src/main/java/org/helpinghands/controllers/HelloWorldController.java
@@ -1,4 +1,4 @@
-package org.helpinghands;
+package org.helpinghands.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
